73734999cae3009d808d6823933b81099e395e3c
aliabb01 <aliabbasov21112001@gmail.com>
2020-03-11 20:43:29 +0200

    Redirects(Middleware and Controllers)

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -2,29 +2,27 @@
 
 namespace App\Http\Middleware;
 
-use App\Providers\RouteServiceProvider;
 use Closure;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 
-class RedirectIfAuthenticated
+class isAdmin
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
+        if(Auth::user()){
             if(Auth::user()->isAdmin){
-                return redirect('admin');
+                return $next($request);
             }
             return redirect('user');
         }
-
-        return $next($request);
+        return redirect('login');
+        
     }
 }
