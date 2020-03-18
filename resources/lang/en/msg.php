1716a3e1956c66ddee927345d19225bdbbc3a38c
aliabb01 <aliabbasov21112001@gmail.com>
2020-03-19 00:45:57 +0200

    Resources(addition of russian language in main menu only with no routes)

---
+++ resources/lang/en/msg.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'welcome' => "Welcome",
+    'home' => "HOME",
+    'about' => "About",
+    'services' => "Services",
+    'feedback' => "Feedback",
+    'contact-us' => "Contact-Us",
+    'login' => "Login",
+    'register' => "Register",
+]
+
+?>
