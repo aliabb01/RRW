e63adb9a02ab0daa5cb74b517c345e9730148ade
aliabb01 <aliabbasov21112001@gmail.com>
2020-04-22 11:56:04 +0300

    Error handling pages (404, 500) + Maintenance mode page update 1

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<title>Apollon Railway</title>
+<style>
+  body {
+    text-align: center;
+    padding: 150px;
+  }
+
+  h1 {
+    font-size: 50px;
+  }
+
+  html,    /* Fills the body inside html tag */
+  body {   
+    margin: 0;
+    height: 100%;
+    background-color:#C2DFFF;
+  }
+
+  body {
+    font: 20px Helvetica, sans-serif;
+    color: #333;
+  }
+
+  article {
+    display: block;
+    text-align: left;
+    width: 45%;
+    margin: 0 auto;
+  }
+
+  a {
+    color: #dc8100;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: #333;
+    text-decoration: none;
+  }
+</style>
+
+<article>
+  <h1>We&rsquo;ll be back soon!</h1>
+  <div>
+    <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can
+      always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+    <p>&mdash; ApollonRailway</p>
+  </div>
+</article>
